8c5304947bdc623c9a19d393f8b0328546f3fba2
777 <goszakupki.aktiv@gmail.com>
2023-01-02 21:26:53 +0300

    yyy

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import aiogram
+# import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN_API = "5978359026:AAEChQXasiulEgf6M02OKm9_ofiyy2B8euI"
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+
+if __name__ == "--name__":
+    executor.start_polling(dp)    #запуск ботов
